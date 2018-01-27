28ccb307f5ab421028df201cee822b9885b8d33b
kmkingdon <32685092+kmkingdon@users.noreply.github.com>
2018-01-27 14:42:15 -0700

    added readme

--- app.js
+++ app.js
@@ -2,57 +2,76 @@ const express = require("express");
 const app = express();
 const queries = require("./queries");
 const bodyParser = require("body-parser");
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/profiles", (request, response) => {
-    queries.list1().then(profiles => {
-        response.json({profiles});
-    }).catch(console.error);
+  queries
+    .list1()
+    .then(profiles => {
+      response.json({ profiles });
+    })
+    .catch(console.error);
 });
 
 app.get("/comments", (request, response) => {
-    queries.list2().then(profiles => {
-        response.json({profiles});
-    }).catch(console.error);
+  queries
+    .list2()
+    .then(profiles => {
+      response.json({ profiles });
+    })
+    .catch(console.error);
 });
 
 app.get("/profiles/:id", (request, response) => {
-    queries.read(request.params.id).then(profiles => {
-        profiles
-            ? response.json({profiles})
-            : response.sendStatus(404)
-    }).catch(console.error);
+  queries
+    .read(request.params.id)
+    .then(profiles => {
+      profiles ? response.json({ profiles }) : response.sendStatus(404);
+    })
+    .catch(console.error);
 });
 
 app.post("/profiles", (request, response) => {
-    queries.create1(request.body).then(profiles => {
-        response.status(201).json({profiles});
-    }).catch(console.error);
+  queries
+    .create1(request.body)
+    .then(profiles => {
+      response.status(201).json({ profiles });
+    })
+    .catch(console.error);
 });
 
 app.post("/comments", (request, response) => {
-    queries.create2(request.body).then(profiles => {
-        response.status(201).json({profiles});
-    }).catch(console.error);
+  queries
+    .create2(request.body)
+    .then(profiles => {
+      response.status(201).json({ profiles });
+    })
+    .catch(console.error);
 });
 
 app.delete("/profiles/:id", (request, response) => {
-    queries.delete(request.params.id).then(() => {
-        response.sendStatus(204);
-    }).catch(console.error);
+  queries
+    .delete(request.params.id)
+    .then(() => {
+      response.sendStatus(204);
+    })
+    .catch(console.error);
 });
 
 app.put("/profiles/:id", (request, response) => {
-    queries.update(request.params.id, request.body).then(profiles => {
-        response.json({profiles});
-    }).catch(console.error);
+  queries
+    .update(request.params.id, request.body)
+    .then(profiles => {
+      response.json({ profiles });
+    })
+    .catch(console.error);
 });
 
 app.use((request, response) => {
-    response.send(404);
+  response.send(404);
 });
 
 module.exports = app;
---
+++ readme.md
@@ -0,0 +1,24 @@
+Welcome to the Harry Potter Magical Network API:
+
+#Database Link:
+
+Deployed Database: https://hpdb.herokuapp.com/
+
+#Paths:
+
+##GET
+All Profile Data:
+https://hpdb.herokuapp.com/profiles
+
+All Profile Comments:
+https://hpdb.herokuapp.com/Comments
+
+Profile By Id:
+https://hpdb.herokuapp.com/profiles/:id
+
+##POST
+Post Profile:
+https://hpdb.herokuapp.com/profiles
+
+Post Comment:
+https://hpdb.herokuapp.com/comments
